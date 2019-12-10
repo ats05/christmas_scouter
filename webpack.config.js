1df24e94266113bddabf261b3d3018dbda3c5090
小野沢　敦 <atsushi_onozawa@waku-2.com>
2019-12-10 17:40:24 +0900

    Initial commit & React最小環境

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        'index': path.resolve(__dirname, 'src/index.jsx'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets/'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, ''),
+        port: 3000,
+    },
+}
