d42654f401e44cda9897de9045a823de656c08c8
小野沢　敦 <atsushi_onozawa@waku-2.com>
2019-12-10 21:49:02 +0900

    一通り作成

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import Score from './score';
 
 export default class Main extends Component {
     constructor(props) {
@@ -8,24 +11,28 @@ export default class Main extends Component {
         let width = window.parent.screen.width;
         let height = window.parent.screen.height;
 
+
         this.state = {
             nowInPlay: true,
             width: width,
-            height: height
+            height: height,
+            model: null,
+            score: "--.-",
         }
+
+        // 変換したモデルを読み込む
+        tf.loadGraphModel('./assets/web_model/model.json').then((model) => {
+            this.setState({model: model})
+            this.animate();
+        })
+
+        this.animate = this.animate.bind(this);
 
     }
     componentDidMount() {
-        console.log(this.refs.videoElement);
         this.videoElement = this.refs.videoElement
-        // this.videoElement.setAttribute('playsinline', true);
-        // this.videoElement.setAttribute('width', width);
-        // this.videoElement.setAttribute('height', height);
-
         navigator.mediaDevices.getUserMedia({
             video: {
-                width: this.state.width,
-                height: this.state.height,
                 aspectRatio: this.state.width / this.state.height, // アスペクト比
                 facingMode:  "environment" // バックカメラを使う指定
             },
@@ -39,9 +46,44 @@ export default class Main extends Component {
             console.log(err);
         });
     }
+    animate() {
+        if (this.state.model !== null) {
+            let tensor = tf.tidy(() => {
+                const channels = 3;
+                let inputImage = tf.browser.fromPixels(this.videoElement, 3);
+                let float_caster = tf.cast(inputImage, 'float32');
+                let dims_expander = float_caster.expandDims(0);
+                let resized = tf.image.resizeBilinear(dims_expander, [299, 299]);
+                let normalized = tf.div(tf.sub(resized, [0]), [255]);
+                return normalized;
+            })
+            new Promise((resolve, reject) => {
+                let result = this.state.model.predict(tensor).array(); // 値はarray()で取り出せる
+                resolve(result);
+            }).then(result => {
+                this.setState({score: Math.round(result[0][0] * 1000) / 10});
+                this.animate(); // 計算が終わってから次の処理が走るようにする
+            });
+        }
+        else {
+            this.animate();
+        }
+    }
     render() {
         return (
-            <video ref="videoElement" width={this.state.width} height={this.state.height} playsInline/>
+            <div>
+                <Video ref="videoElement" width={this.state.width} height={this.state.height} playsInline/>
+                <Score score={this.state.score} />
+            </div>
         );
     }
 }
+
+const Video = styled.video`
+width: 100%;
+max-width: 100%;
+height: 100%;
+object-fit: fill;
+display: block;
+margin: 0 auto;
+`
